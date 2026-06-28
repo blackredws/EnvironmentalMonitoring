3b680a28a25bc20530b665fae67d98646deb82c5
surafel hunegnaw babl <140015439+blackredws@users.noreply.github.com>
2026-06-28 16:51:27 +0200

    Add files via upload

---
+++ sensors.py
@@ -0,0 +1,99 @@
+SENSORS = [
+
+    {
+        "sensor_id": "S01",
+        "building": "Engineering",
+        "room": "Lab A",
+        "manufacturer": "Bosch",
+        "gateway": "Gateway01"
+    },
+
+    {
+        "sensor_id": "S02",
+        "building": "Engineering",
+        "room": "Lab A",
+        "manufacturer": "Honeywell",
+        "gateway": "Gateway01"
+    },
+
+    {
+        "sensor_id": "S03",
+        "building": "Engineering",
+        "room": "Lab B",
+        "manufacturer": "Bosch",
+        "gateway": "Gateway01"
+    },
+
+    {
+        "sensor_id": "S04",
+        "building": "Engineering",
+        "room": "Lab B",
+        "manufacturer": "Siemens",
+        "gateway": "Gateway01"
+    },
+
+    {
+        "sensor_id": "S05",
+        "building": "Science",
+        "room": "Room 101",
+        "manufacturer": "Bosch",
+        "gateway": "Gateway02"
+    },
+
+    {
+        "sensor_id": "S06",
+        "building": "Science",
+        "room": "Room 102",
+        "manufacturer": "Honeywell",
+        "gateway": "Gateway02"
+    },
+
+    {
+        "sensor_id": "S07",
+        "building": "Science",
+        "room": "Room 103",
+        "manufacturer": "Bosch",
+        "gateway": "Gateway02"
+    },
+
+    {
+        "sensor_id": "S08",
+        "building": "Science",
+        "room": "Lab C",
+        "manufacturer": "Siemens",
+        "gateway": "Gateway02"
+    },
+
+    {
+        "sensor_id": "S09",
+        "building": "Agriculture",
+        "room": "Greenhouse",
+        "manufacturer": "Bosch",
+        "gateway": "Gateway03"
+    },
+
+    {
+        "sensor_id": "S10",
+        "building": "Agriculture",
+        "room": "Greenhouse",
+        "manufacturer": "Honeywell",
+        "gateway": "Gateway03"
+    },
+
+    {
+        "sensor_id": "S11",
+        "building": "Agriculture",
+        "room": "Storage",
+        "manufacturer": "Siemens",
+        "gateway": "Gateway03"
+    },
+
+    {
+        "sensor_id": "S12",
+        "building": "Agriculture",
+        "room": "Storage",
+        "manufacturer": "Bosch",
+        "gateway": "Gateway03"
+    }
+
+]
